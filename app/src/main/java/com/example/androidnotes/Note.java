0b2ddf5032c2b897131f8bf27f2e37b8419d84a5
shlomi <shl.lev.ks@gmail.com>
2021-09-04 21:03:26 +0300

    init + add note entity + room gradle dep

---
+++ app/src/main/java/com/example/androidnotes/Note.java
@@ -0,0 +1,37 @@
+package com.example.androidnotes;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+    private String description;
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
